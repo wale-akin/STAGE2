a0df342ac09757d284330eb778c47aa2b78fd54c
Wale Akinbolagbe <aakinbolagbe@gmail.com>
2023-02-02 08:42:24 -0800

    removed idea and ds

--- code/stage_2_code/Evaluate_Accuracy.py
+++ code/stage_2_code/Evaluate_Accuracy.py
@@ -15,4 +15,8 @@ class Evaluate_Accuracy(evaluate):
     def evaluate(self):
         print('evaluating performance...')
         return accuracy_score(self.data['true_y'], self.data['pred_y'])
-        
+
+# try and implment a new function with f1 versions
+# could i call f1 and try and print in evaluate
+# probably would have to test different evaluation method one by one unless
+# i find a way to return multiple evals at once
